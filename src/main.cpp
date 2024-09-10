6f91057fd61ccdc043ef685436e8f622a49e0725
sam11235895 <sam11235895@gmail.com>
2024-09-10 11:20:25 +0800

    capture picture

--- src/main.cpp
+++ src/main.cpp
@@ -48,8 +48,43 @@ FirebaseConfig configF;
 
 void capturePhotoSaveLittleFS(void)
 {
+  camera_fb_t *fb = NULL;
+  for(int i =0; i<4;i++)
+  {
+    fb = esp_camera_fb_get();
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
+  fb = NULL;
+  fb=esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Camera capture failed!");
+    delay(1000);
+    ESP.restart();
+  }
+  Serial.printf("Picture file name: %s\n", FILE_PHOTO_PATH);
+  File file = LittleFS.open(FILE_PHOTO_PATH,FILE_WRITE);
+
+  if (!file)
+  {
+    Serial.println("Failed tp open file in writing mode !!");
+  }
+  else
+  {
+    file.write(fb->buf,fb->len);
+    Serial.print("The picture has been save in");
+    Serial.print(FILE_PHOTO_PATH);
+    Serial.print(" - Size: ");
+    Serial.print(fb ->len);
+    Serial.println("bytes");
+  }
+  file.close();
+  esp_camera_fb_return(fb);
+  
   
 }
+
 
 void initWiFi()
 {
@@ -137,4 +172,5 @@ void loop() {
   {
     capturePhotoSaveLittleFS();
   }
+  TakeNewPhoto=false;
 }
